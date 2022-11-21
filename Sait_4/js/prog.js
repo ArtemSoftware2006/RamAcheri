6817196317e17d52dc31fae62d73f43f4130b84d
Artem Egorov <94646826+ArtemSoftware2006@users.noreply.github.com>
2022-11-21 13:31:45 +0300

    Add files via upload
    Сайт новостей

---
+++ Sait_4/js/prog.js
@@ -0,0 +1,46 @@
+"use strict"
+
+let a = "Hello, world", 
+count = 10,
+IsTrue = true;
+
+const PI = 3.14;
+const age = count + 9
+
+console.log(count * PI);
+console.log(age);
+
+if(IsTrue)
+{
+    var str = "Perfect"
+}
+console.log(str);
+
+let Undefind = 90 ;
+
+if (Undefind == undefined)
+{
+    console.log("Нет значения");
+}
+else
+{
+    console.log(typeof(Undefind))
+}
+
+let asr = document.querySelectorAll('.BLOSF');
+console.log(asr);
+console.log(typeof(asr));
+
+const fred = 14237557345754329479325n;
+console.log(`Возраст гаходки ${fred} лет`);
+
+var d = "234";
+console.log(d);;
+console.log(typeof(d));
+d = Number(d);
+console.log(typeof(d));
+console.log(d);
+
+
+let g = "45" / "34";
+console.log(typeof(g));
